6207b87af90596201bdee1d3463a54bda0dcb04e
Amos-e <amosemmanelz@gmail.com>
2025-02-14 12:25:57 +0300

    Updated inverse

--- inverse_image.py
+++ inverse_image.py
@@ -1,9 +1,9 @@
 import cv2 as cv
 import numpy as np
 
-image_path = 'my_image.py'
+image_path = 'my_image.jpeg'
 
-img = cv.imread(image_path,0)
+img = cv.imread(image_path, 0)
 img = cv.resize(img, (500,500))
 
 #Image Inverse
@@ -12,10 +12,17 @@ inverse = np.linalg.inv(img)
 #Identity matrix for image
 identity = np.multiply(img, inverse)
 
+#Transpose 1
 transpose = img.transpose()
+
+#Transpose 2
 transpose2 = transpose.transpose()
 
+#Display Images
+cv.imshow("Inverse Image", inverse)
+cv.imshow("Identity Image", identity)
+cv.imshow("Transpose 1", transpose)
+cv.imshow("Transpose 2", transpose2)
 
-cv.imshow("Image", transpose2)
 cv.waitKey(0)
 cv.destroyAllWindows()
